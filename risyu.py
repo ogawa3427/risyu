cd00aa00086ab5d2123b7196ee8fd1b17ca5dc9b
Ogawa <rst34shimizuku@stu.kanazawa-u.jp.ac>
2023-09-21 02:22:02 +0900

    daitai

--- risyu.py
+++ risyu.py
@@ -1,37 +1,56 @@
 import os
 import re
 
+header = "時間割番号,科目区分,時間割名,曜日時限,教員名,対象学生,適正人数,全登録数,優先指定,第１希望,第２希望,第３希望,第４希望,第５希望"
 #dir,csvありますかチェック
 if os.path.exists("dir.csv"):
-    print ('aru')
     with open('dir.csv', "r", encoding='utf-8') as dir:
         path = dir.readline()
         two = dir.readline()
         if two:
             print ('Error!Check your dir.csv')
         filename = os.path.join(path, "金沢大学教務システム - 抽選科目登録状況.htm")
-
 else:
-    print ('nai')
     filename = os.path.join(os.environ["HOME"], "金沢大学教務システム - 抽選科目登録状況.htm")
-
-print (filename)
 
 with open(filename, 'r', encoding='utf-8') as f:
     line = f.readline()
     mode = 0
+    #turn = 1
+    content = ""
     while line:
-        if '						</tbody></table>' in line:
+        if '                        </tbody></table>' in line:
             mode = 2
         if mode == 1:
-            print (line, end="")
-        if '						<table' in line:
+            if "</tr><tr" in line:
+                line = "eskape"
+            content += line
+        if 'th align' in line:
             mode = 1
- 
+        line = f.readline()
+        #print (turn)
+        #turn += 1
+    content = content.replace(" ", "")
+    content = content.replace("\t", "")
+    content = content.replace("\n", "")
+    content = content.replace("</span>", "")
+    content = re.sub(r'<span.*?>', '', content)
+    content = re.sub(r'<td.*?>', '<td>', content)
+    content = content.replace("</td><td>", ",")
+    content = content.replace("<td>", "")
+    content = content.replace("</td>", "")
+    content = content.replace("&amp;", "&")
+    content = re.sub(r'<\/tr>.*', '', content)
+
+    content = re.sub(r'^\n', '', content)
+    content = content.replace("eskape", "\n")
+    content = header + content
+
+print (content)
 
 """
 # Extract name
-name_pattern = r'ctl00_phContents_ucRegistrationStatus_lblDate.*?>([^<]+)<'
+name_pattern = r'ctl00_phContents_ucRegicontentationStatus_lblDate.*?>([^<]+)<'
 name_match = re.search(name_pattern, content)
 if name_match:
     name = re.sub(r'[ :/]', '', name_match.group(1))
