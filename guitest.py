45f2069461d284fe8584b1a3faa59408dddd13d9
Ogawa <rst34shimizuku@stu.kanazawa-u.jp.ac>
2023-09-21 23:11:34 +0900

    ﾁｶﾚﾀ

--- guitest.py
+++ guitest.py
@@ -3,17 +3,22 @@ import re
 import PySimpleGUI as sg
 compl=""
 header = "時間割番号,科目区分,時間割名,曜日時限,教員名,対象学生,適正人数,全登録数,優先指定,第１希望,第２希望,第３希望,第４希望,第５希望"
-defpath = ""
+with open('dir.csv', "r", encoding='utf-8') as dir:
+		defpath = dir.readline()
 
-layout = [[sg.Text('金沢大学教務システム - 抽選科目登録状況.htmが入っているディレクトリのフルパスを入力')], 
+layout1 = [
+[sg.Text('金沢大学教務システム - 抽選科目登録状況.htmが入っているディレクトリのフルパスを入力')], 
 [sg.Text('パスを保存しておらず未入力の場合は$HOMEを見ます')], 
 [sg.Checkbox("入力したパスを保存する", key="-SAVE-", default=True)], 
 [sg.InputText(key='-INP-', default_text=defpath), sg.Button('実行', key='-SUBMIT-')], 
-[sg.Text(compl)]]
-window = sg.Window('risyu', layout, size=(700,600))
+[sg.Text(compl, key='-COMPL-')], 
+[sg.Text('', key='-ERROR-', size=(30, 1), text_color='red')], 
+[sg.Button('(最新版を元に)表示', key='-NEXT-')]
+]
+window1 = sg.Window('risyu', layout1, size=(700,600))
 
 while True:
-	event, values = window.read()
+	event, values = window1.read()
 	if event=='-SUBMIT-':
 
 #HomeOrOther
@@ -39,7 +44,16 @@ while True:
 			if values['-SAVE-'] == True:
 				with open('dir.csv', 'w', encoding='utf-8') as file:
 					file.write(path)
+		if not os.path.exists(filename):
+			window1['-ERROR-'].update('指定されたファイルが存在しません')
+			continue
 	elif event==sg.WIN_CLOSED:
+		sta = 0
+		break
+
+	elif event=='-NEXT-':
+		sta = 1
+		window1.close()
 		break
 
 	with open(filename, 'r', encoding='utf-8') as f:
@@ -86,5 +100,30 @@ while True:
 
 	with open(name, 'w', encoding='utf-8') as file:
 		file.write(content)
-	print('comp')
-	break
+	compl = "保存しました" + name
+	window1['-COMPL-'].update(compl)
+	window1['-ERROR-'].update('')
+
+
+layout2 = [
+[sg.Text('金沢大学教務システム - 抽選科目登録状況.htmが入っているディレクトリのフルパスを入力')], 
+[sg.Checkbox("GSのみ", key="-ONLYGS-", default=True)], 
+[sg.Button('月1'), sg.Button('火1'), sg.Button('水1'), sg.Button('木1'), sg.Button('金1')], 
+[sg.Button('月2'), sg.Button('火2'), sg.Button('水2'), sg.Button('木2'), sg.Button('金2')], 
+[sg.Button('月3'), sg.Button('火3'), sg.Button('水3'), sg.Button('木3'), sg.Button('金3')], 
+[sg.Button('月4'), sg.Button('火4'), sg.Button('水4'), sg.Button('木4'), sg.Button('金4')], 
+[sg.Button('月5'), sg.Button('火5'), sg.Button('水5'), sg.Button('木5'), sg.Button('金5')], 
+[sg.Button('6限'), sg.Button('7限'), sg.Button('8限'), sg.Button('集中')],
+[sg.Text(compl, key='-COMPL-')], 
+[sg.Text('', key='-ERROR-', size=(30, 1), text_color='red')], 
+[sg.Button('(最新版を元に)表示', key='-NEXT-')]
+]
+window2 = sg.Window('risyu', layout2, size=(700,600))
+
+while True:
+	event, values = window2.read()
+	if event == sg.WIN_CLOSED:
+		break
+	else:
+		buttontext = event
+		print(buttontext)
