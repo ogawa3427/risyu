29a12c9bd90aa02d0b6fd1c85fb6eb765d851327
Ogawa <rst34shimizuku@stu.kanazawa-u.jp.ac>
2023-10-07 01:52:04 +0900

    まあ読みはできんじゃねえの

--- main.py
+++ main.py
@@ -5,21 +5,21 @@ import re
 
 @app.route('/')
 def index():
-    base_dir = os.path.dirname(os.path.abspath(__file__))
-    file_path = os.path.join(base_dir, 'risyu20230924235801.csv')
-    asof = '2023年何月24日hh:mm現在'
+	base_dir = os.path.dirname(os.path.abspath(__file__))
+	file_path = os.path.join(base_dir, 'risyu20230924235801.csv')
+	asof = '2023年何月24日hh:mm現在'
 
-    
-    with open(file_path, 'r') as f:
-        theline = ''.join(line for line in f if "時間割番号,科目区分" not in line)
-        theline = re.sub('\n','eskape', theline)
-        pront(theline)
-    
-    return render_template(
-        'index.html',
-        theline=theline,
-        asof=asof
-    )
+	
+	with open(file_path, 'r') as f:
+		theline = ''.join(line for line in f if "時間割番号,科目区分" not in line)
+		theline = re.sub('\n','eskape', theline)
+		pront(theline)
+	
+	return render_template(
+		'index.html',
+		theline=theline,
+		asof=asof
+	)
 @app.route('/set')
 def set():
 	return render_template(
@@ -27,7 +27,9 @@ def set():
 		)
 @app.route('/aff')
 def aff():
+	rolelist = ["全学生","全学生（地域創造学類以外）","全学生（理工学域優先）"]
 	return render_template(
 		'aff.html',
-        sver='2023Q3'
+		srole='0000000201330023',
+		rolelist=rolelist
 		)
