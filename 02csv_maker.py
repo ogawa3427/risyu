067175c7f5b7d00346b184aa42befc45686918ed
Ogawa <135859092+ogawa3427@users.noreply.github.com>
2023-11-13 18:15:33 +0900

    6parts5machine

---
+++ 02csv_maker.py
@@ -0,0 +1,85 @@
+import re
+import os
+csvs_directory = os.path.join(os.getcwd(), 'csvs')
+
+import json
+import csv
+import time
+
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+smtpserver = 'smtp.gmail.com'
+smtpport = 587
+
+server = smtplib.SMTP(smtpserver, smtpport)
+server.starttls()
+server.login(os.environ["GMAIL"], os.environ["GMAILPASS"])
+
+text = '🦝🦝🦝🦝🦝CSVMaker_ERROR!!!!!!!!!!'
+msg = MIMEText(text)
+msg['Subject'] = '🦝🦝🦝🦝🦝CSVERROR!!!!!!!!!!'
+msg['From'] = os.environ["GMAIL"]
+msg['To'] = os.environ["GMAIL"]
+msg['date'] = time.strftime('%a, %d %b %Y %H:%M:%S %z')
+
+source = "金沢大学教務システム - 抽選科目登録状況.htm"
+#source = 'page_source.html'
+
+while True:
+	try:
+		with open(source, 'r', encoding='utf-8') as f:
+			line = f.readline()
+			mode = 0
+			content=''
+			name = ''
+			while line:
+				if "ctl00_phContents_ucRegistrationStatus_lb" in line:
+					name = line
+				if '						</tbody></table>' in line:
+					mode = 2
+				if mode == 1:
+					if "</tr><tr" in line:
+						line = "eskape"
+					content += line
+				if 'th align' in line:
+					mode = 1
+				line = f.readline()
+
+		content = content.replace(" ", "")
+		content = content.replace("\t", "")
+		content = content.replace("\n", "")
+		content = content.replace("</span>", "")
+		content = re.sub(r'<span.*?>', '', content)
+		content = re.sub(r'<td.*?>', '<td>', content)
+		content = content.replace("</td><td>", ",")
+		content = content.replace("<td>", "")
+		content = content.replace("</td>", "")
+		content = content.replace("&amp;", "&")
+		content = re.sub(r'<\/tr>.*', '', content)
+
+		content = re.sub(r'^\n', '', content)
+		content = content.replace("eskape", "\n")
+		#header = "時間割番号,科目区分,時間割名,曜日時限,教員名,対象学生,適正人数,全登録数,優先指定,第１希望,第２希望,第３希望,第４希望,第５希望"
+		#contents = header + '\n' + content
+		contents = content
+
+		name = re.sub(r'<.*?>', '', name)
+		name = re.sub(r'\t', '', name)
+		name = re.sub(r'[ :\/]', '', name)
+		name = re.sub(r'\n', '', name)
+		name = "risyu" + name
+		name = name + ".csv"
+		
+		path = os.path.join(csvs_directory, name)
+		with open(path, 'w', encoding='utf-8') as file:
+			file.write(contents)
+
+		print('CSV_made_sucsessfuly')
+
+		time.sleep(55)
+	except:
+		server.send_message(msg)
+		server.close()
+		print('CSV_error')
+
--- test.py
+++ test.py
@@ -31,97 +31,14 @@ msg['date'] = time.strftime('%a, %d %b %Y %H:%M:%S %z')
 
 
 try:
-    os.mkdir('screenshots')
-# Setup Chrome options
-    options = Options()
-    #options.add_argument("--headless") # Ensure GUI is off. Remove this line if you want to see the browser navigating.
-    
-    # Set path to chromedriver as a service
-    webdriver_service = Service(ChromeDriverManager().install())
-    
-    # Set the driver
-    driver = webdriver.Chrome(service=webdriver_service, options=options)
-    
-    # Get website data
-    driver.get("https://acanthus.cis.kanazawa-u.ac.jp/?lan=j")
-    
-    link = driver.find_element(By.LINK_TEXT, "ログイン")
-    link.click()
-    
-    # Take a screenshot
-    driver.implicitly_wait(5)
-    driver.save_screenshot("screenshot.png")
-    
-    kuid = driver.find_element(By.ID, "kuid")
-    passw = driver.find_element(By.ID, "password")
-    
-    kuid.clear()
-    passw.clear()
-    
-    KUID = os.environ["KUID"]
-    PASS = os.environ["KUPASS"]
-    kuid.send_keys(KUID)
-    passw.send_keys(PASS)
-    
-    driver.get_screenshot_as_file("screenshots2.png")
-    
-    driver.find_element(By.NAME, "_eventId_proceed").click()
-    # Quit the driver
-    
-    driver.get_screenshot_as_file("screenshots3.png")
-    
-    #link = driver.find_element(By.LINK_TEXT, "既に回答しました")
-    #link.click()
-    
-    element = driver.find_element(By.XPATH,"//a[span[text()='学務情報サービス']]")
-    driver.execute_script("arguments[0].click();", element)
-    
-    all_handles = driver.window_handles
-    
-    driver.switch_to.window(all_handles[-1])
-    
-    print(driver.title)
-    
-    driver.get_screenshot_as_file("screenshots4.png")
-    
-    link = driver.find_element(By.XPATH, "//a[contains(., '履修・成績情報')]")
-    link.click()
-    
-    driver.get_screenshot_as_file("screenshots5.png")
-    
-    link = driver.find_element(By.XPATH, "//a[contains(., '履修登録')]")
-    link.click()
-    
-    driver.get_screenshot_as_file("screenshots7.png")
-    
-    print(1/0)
-    
-    ##謎
-    #link = driver.find_element(By.XPATH, "//a[contains(., '履修登録状況')]")
-    #link.click()
-    
-    #状況クリック後
-    #0で全件表示
-    #select_element = driver.find_element(By.ID, "ctl00_phContents_ucRegistrationStatus_ddlLns_ddl")
-    #select_object = Select(select_element)
-    #select_object.select_by_value("0")
-    
-    #pdfurl = driver.current_url
-    #driver.get(pdfurl)
-    
-    
-    #page_source = driver.page_source
-    #with open('page_source.html', 'w', encoding='utf-8') as f:
-    #    f.write(page_source)
-    
-    #driver.close()
-    #driver.refresh()
-    
-    time.sleep(1000)
-    driver.quit()
+    while True:
+        with open('watchtest.txt', 'r', encoding='utf-8') as f:
+            watchtest = f.read()
+        print(watchtest)
+
+        time.sleep(1)
 
 except:
-    driver.quit()
-    
-    server.send_message(msg)
-    server.close()
+    #server.send_message(msg)
+    #server.close()
+    print('error')
