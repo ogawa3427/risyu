7380f1a3ac2e69905f8a55d501a7852d8beebd10
Ogawa <135859092+ogawa3427@users.noreply.github.com>
2024-03-03 01:11:24 +0900

    他学類動いてくれ

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import json
 import csv
 import time
 from flask import jsonify
+from flask import request
 from markupsafe import Markup
 
 app = Flask(__name__)
@@ -16,6 +17,45 @@ app.json.ensure_ascii = False
 csvs_directory = os.path.join(os.path.expanduser('~'), 'risyu', 'csvs')
 
 count = 0
+
+jb_dict = {
+    '51_00': '学域共通',
+    '51_01': '人文学類',
+    '51_02': '法学類',
+    '51_03': '経済学類',
+    '51_04': '学校教育学類,学校教育学類共同教員養成課程',
+    '51_05': '地域創造学類',
+    '51_06': '国際学類',
+    '51_99': '教職・資格'
+}
+
+rk_dict = {
+    '52_00': '学域共通',
+    '52_10': '理工一括',
+    '52_01': '数物科学類',
+    '52_02': '物質化学類',
+    '52_11': '機械工学類',
+    '52_12': 'フロンティア工学類',
+    '52_13': '電子情報通信学類',
+    '52_14': '地球社会基盤学類',
+    '52_15': '生命理工学類',
+    '52_99': '教職・資格'
+}
+
+md_dict = {
+    '53_00': '学域共通',
+    '53_01': '医学類',
+    '53_02': '保健学類',
+    '53_04': '薬学類',
+    '53_95': '医薬科学類'
+}
+
+other_dict = {
+    '00_00': '総合教育部',
+    '--_--': '共通教育',
+    '46_91': '国際機構'
+}
+
 
 with open(os.path.join(os.path.expanduser('~'), 'risyu', 'sv_admin', 'strodict.json'), 'r', encoding='utf-8') as f:
     strodict = json.load(f)
@@ -136,20 +176,68 @@ def tests():
 
 @app.route('/yugo_table')
 def yugo_table():
+    iswith = request.args.get('yugo')
+    aff = request.args.get('aff')
+    iki = ""
+
+    if "51_" in aff:
+        iki = "人間社会学域"
+        p_rui = jb_dict[aff]
+    elif "52_" in aff:
+        iki = "理工学域"
+        p_rui = rk_dict[aff]
+    elif "53_" in aff:
+        iki = "医薬保健学域"
+        p_rui = md_dict[aff]
+    else:
+        iki = "other"
+        p_rui = other_dict[aff]
+
+    filled = {}
+    if "_00" in aff:
+        rui = "学域共通"
+        for key in course_info:
+            if course_info[key]['iki'] == iki and course_info[key]['rui'] == rui:
+                filled[key] = course_info[key]
+    elif iki == "other":
+        for key in course_info:
+            print("!--")
+            print(course_info[key]['rui'])
+            print(rui)
+            if course_info[key]['iki'] == p_rui:
+                filled[key] = course_info[key]
+    else:
+        ruis = p_rui.split(",")
+        for rui in ruis:
+            for key in course_info:
+                print("---")
+                print(course_info[key]['rui'])
+                print(rui)
+                if course_info[key]['rui'] == rui:
+                    filled[key] = course_info[key]
+        #for key in course_info:
+        #    if course_info[key]['iki'] == iki and course_info[key]['rui'] == aff:
+        #        filled[key] = course_info[key]
+
+    if iswith == "1":
+        for key in course_info:
+            if course_info[key]['iki'] == "融合学域":
+                filled[key] = course_info[key]
+    
     # course_infoをJSON文字列に変換
-    course_info_json = json.dumps(course_info, ensure_ascii=False)
-    # JSON文字列を安全なマークアップとしてマーク
-    to_send = {}
-    for key in course_info:
-        if course_info[key]['iki'] == '融合学域' or course_info[key]['rui'] == '電子情報通信学類':
-            to_send[key] = course_info[key]
-    to_send_json = json.dumps(to_send, ensure_ascii=False)
+    to_send_json = json.dumps(filled, ensure_ascii=False)
+
     course_info_safe = Markup(to_send_json)
     course_info_safe = course_info_safe.replace('^\"', "")
     course_info_safe = course_info_safe.replace("\"$", "")
+
+    print(iki)
+    print(p_rui)
+
     return render_template(
         'yugo_table.html',
-        course_info=course_info_safe
+        course_info=course_info_safe,
+        rui_name=p_rui
     )
 
 if __name__ == "__main__":
