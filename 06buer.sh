6164e205b93d9109257b39ee1c4f51f553738502
Ogawa <135859092+ogawa3427@users.noreply.github.com>
2024-04-01 07:24:32 +0900

    ほんばん

--- 06buer.sh
+++ 06buer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true; do
-    asof=$(curl  http://20.78.138.195:5000/api | sed 's/","/\n/g' | grep "asof" | sed 's/[^0-9]//g' | sed 's/$/.csv/')
+    asof=$(curl https://kurisyushien.org/api | sed 's/\\n/\n/g' | grep "valid" | sed 's/ //g' | sed 's|/||g' | sed 's/\\tvalid//g')
     echo $asof
-    curl http://20.78.138.195:5000/api > ./csvs/"$asof"
+    curl https://kurisyushien.org/api > ./csvs/"$asof"
     sleep 51
 done
